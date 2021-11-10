4203875fef0adbf87de2327afa7a89a4acec71c8
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-11-10 17:40:06 -0500

    typo in dayjs import

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import LocalizedFormat from 'dayjs/plugin/LocalizedFormat';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 
 dayjs.extend(relativeTime);
 dayjs.extend(LocalizedFormat);
