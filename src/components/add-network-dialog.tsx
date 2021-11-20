91a4a8bf58a88063b97957217c10ca9933809877
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-11-20 11:38:12 -0500

    fix: improve add network dialog

--- src/components/add-network-dialog.tsx
+++ src/components/add-network-dialog.tsx
@@ -5,6 +5,9 @@ import { Formik, Form, Field } from 'formik';
 import { TextField } from 'formik-mui';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
+import Box from '@mui/system/Box';
+import Stack from '@mui/material/Stack';
+
 //import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
@@ -25,7 +28,7 @@ const AddNetworkDialog = () => {
   return (
     <>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add Network</DialogTitle>
+        <DialogTitle>{t`Add Network`}</DialogTitle>
         <DialogContent>
           <DialogContentText>
             {t`Add and select a new Stacks Blockchain API node using this form.`}
@@ -55,11 +58,10 @@ const AddNetworkDialog = () => {
               const url = new URL(values.url);
               // TODO: run an info query to discover the chainMode
               //await setChainMode(networkMode);
-              // TODO: handle localhost / http://
               void handleAddNetwork({
                 name: values.name.trim(),
                 label: url.host,
-                url: `https://${url.host}`,
+                url: values.url,
                 chain: 'testnet',
               });
               setOpen(false);
@@ -71,33 +73,53 @@ const AddNetworkDialog = () => {
           >
             {({ submitForm, isSubmitting }) => (
               <Form>
-                <Field
-                  component={TextField}
-                  name="name"
-                  type="name"
-                  label="Name"
-                  variant="outlined"
-                  fullWidth
-                  placeholder={t`My Stacks Node`}
-                />
-                <br />
-                <Field
-                  component={TextField}
-                  type="url"
-                  label="url"
-                  name="url"
-                  variant="outlined"
-                  fullWidth
-                  placeholder="https://"
-                />
-                {isSubmitting && <LinearProgress />}
-                <br />
-                <Button color="primary" disabled={isSubmitting} onClick={handleClose}>
-                  {t`Cancel`}
-                </Button>
-                <Button color="primary" disabled={isSubmitting} onClick={submitForm}>
-                  {t`Add and Select`}
-                </Button>
+                <Box
+                  sx={{
+                    mb: 4,
+                    justifyContent: 'center',
+                    gap: 2,
+                  }}
+                >
+                  <Stack
+                    borderRadius={4}
+                    maxWidth="sm"
+                    sx={{ m: 'auto', mb: 1, p: 4, bgcolor: 'background.paper' }}
+                    spacing={4}
+                  >
+                    <Field
+                      component={TextField}
+                      name="name"
+                      type="name"
+                      label="Name"
+                      variant="outlined"
+                      fullWidth
+                      placeholder={t`My Stacks Node`}
+                    />
+                    <Field
+                      component={TextField}
+                      type="url"
+                      label="url"
+                      name="url"
+                      variant="outlined"
+                      fullWidth
+                      placeholder="https://"
+                    />
+                    {isSubmitting && <LinearProgress />}
+                    <Stack direction="row">
+                      <Button color="primary" disabled={isSubmitting} onClick={handleClose}>
+                        {t`Cancel`}
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={isSubmitting}
+                        onClick={submitForm}
+                      >
+                        {t`Add and Select`}
+                      </Button>
+                    </Stack>
+                  </Stack>
+                </Box>
               </Form>
             )}
           </Formik>
